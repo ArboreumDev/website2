092b055adfc82071b562342fc667e3d2de9b3d95
viswaah <vishwajenvish@gmail.com>
2022-10-06 20:13:54 +0530

    add conditional rendering orientation

--- src/Vision/index.tsx
+++ src/Vision/index.tsx
@@ -1,11 +1,30 @@
+import { useState, useEffect } from 'react';
 import LandscapeVision from './LandscapeVision';
 import Header from '../Header';
+import PortraitVision from './PortraitVision';
 
 function Vision() {
+	const [orientation, setOrientation] = useState<
+		'portrait' | 'landscape' | null
+	>(null);
+	useEffect(() => {
+		const changeOrientationHandler = () => {
+			let vh = window.innerHeight * 0.01;
+			let vw = window.innerWidth * 0.01;
+			document.documentElement.style.setProperty('--vh', `${vh}px`);
+			document.documentElement.style.setProperty('--vw', `${vw}px`);
+			if (vh > vw) setOrientation('portrait');
+			else setOrientation('landscape');
+		};
+		window.addEventListener('resize', changeOrientationHandler);
+		return () => {
+			window.removeEventListener('resize', changeOrientationHandler);
+		};
+	});
 	return (
 		<>
 			<Header />
-			<LandscapeVision />
+			{orientation === 'landscape' ? <LandscapeVision /> : <PortraitVision />}
 		</>
 	);
 }
